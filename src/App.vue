41fa436a08df643899eab540a0252946686af208
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2020-08-09 11:39:13 +0200

    Adding Teleport, Composition API, scoped slot styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <button @click="toggleModalState">Open modal</button>
-  <modal v-if="modalOpen">
-    <p>Hello, I'm a modal window.</p>
-  </modal>
+  <teleport to="#modal-wrapper">
+    <modal v-if="modalOpen" @close="toggleModalState">
+      <p>Hello, I'm a modal window.</p>
+    </modal>
+  </teleport>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import Modal from "./components/Modal.vue";
+import { ref } from "vue";
 
 export default {
   name: "App",
@@ -16,13 +19,15 @@ export default {
     HelloWorld,
     Modal,
   },
-  data: () => ({
-    modalOpen: false,
-  }),
-  methods: {
-    toggleModalState() {
-      this.modalOpen = !this.modalOpen;
-    },
+  setup() {
+    const modalOpen = ref(false);
+    const toggleModalState = () => {
+      modalOpen.value = !modalOpen.value;
+    };
+    return {
+      modalOpen,
+      toggleModalState,
+    };
   },
 };
 </script>
